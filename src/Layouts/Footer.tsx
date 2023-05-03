462865181d16f23461cd4e73689c937f34aa2391
david temitope <temi236topmicheal@gmail.com>
2023-05-03 04:45:20 +0100

    first commit \

--- src/Layouts/Footer.tsx
+++ src/Layouts/Footer.tsx
@@ -10,8 +10,8 @@ const Footer = () => {
   return (
     <footer className=" bg-blue-900 flex flex-col items-center justify-center pt-8 px-3 lg:pt-20">
       <div className=" flex flex-col md:flex-row-reverse  md:max-w-[83%] mx-auto gap-10 ">
-        <div className="flex px-8 md:px-0 justify-between md:justify-around md:flex-1 max-sm:text-sm  text-white font-light md:gap-10 ">
-          <ul className="space-y-8 text-md md:text-base">
+        <div className="flex px-6 md:px-0 justify-between md:justify-around md:flex-1 max-sm:text-sm  text-white font-light md:gap-10 ">
+          <ul className="space-y-8 text-md md:text-base grid place-items-center">
             <h4 className="font-bold text-md">Company</h4>
             <li>About us</li>
             <li>About us</li>
@@ -25,12 +25,12 @@ const Footer = () => {
             <li>+234 801 2323 332</li>
           </ul>
         </div>
-        <div className=" px-4 mt-6 sm:mt-0 md:w-3/6 h-full ">
+        <div className=" px-4 mt-4 sm:mt-0 md:w-3/6 h-full ">
           <img
             src={meerasfooter}
             className=" h-20 w-20 bg-no-repeat bg-contain "
           ></img>
-          <p className="lg:w-2/3 mt-2 text-md sm:text-xl text-white font-light md:text-base">
+          <p className="lg:w-2/3 mt-6 text-md sm:text-xl text-white font-light md:text-base">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare
             cursus sed nunc eget dictum Sed ornare cursus sed nunc eget dictumd
             nunc eget dictum Sed ornare cursus sed nunc eget dictum
