41e17e75f27e9b76d824bfaca01e71463e9c1c47
david temitope <temi236topmicheal@gmail.com>
2023-05-03 06:16:29 +0100

    some fix

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,7 +3,7 @@ import aboutlogo from "../About/assets/images/AboutPagehero.png";
 
 const About = () => {
   return (
-    <main className="h-[100vh] pt-20">
+    <main className="pt-20">
       <section className="  bg-white  p-4 lg:py-10 ">
         <div className="w-full h-full relative mx-auto flex flex-col  lg:flex-row   sm:w-[80%] md:w-[70%]  md:mt-14  lg:mt-0 lg:w-[90%] lg:p-20 lg:gap-1 ">
           <div className="text-center mt-20 space-y-5 sm:space-y-7 md:space-y-10 lg:space-y-4 xl:space-y-5 md:mt-4 lg:text-left lg:w-1/2 ">
