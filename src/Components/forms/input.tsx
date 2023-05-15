3c7e03d90964227f96d2224b0989a59194a7698a
david temitope <temi236topmicheal@gmail.com>
2023-05-15 10:35:51 +0100

    test

--- src/Components/forms/input.tsx
+++ src/Components/forms/input.tsx
@@ -3,33 +3,33 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "../../utils/tailwindclassmod";
 
 const inputVariants = cva(
-    " mx-auto p-2 capitalize border-blue-400 border-[3px] rounded-md inline-flex focus:border-blue-400 outline-none w-[100%]",
-    {
-        variants: {
-            variant: {
-                default: "bg-[#EAEAEA] text-black",
-            },
-        },
-        defaultVariants: {
-            variant: "default",
-        },
-    }
+  " mx-auto p-2 capitalize border-blue-400 border-[3px] rounded-md inline-flex focus:border-blue-400 outline-none w-[100%]",
+  {
+    variants: {
+      variant: {
+        default: "bg-[#EAEAEA] text-gray-900",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
 );
 interface InputProps
-    extends InputHTMLAttributes<HTMLInputElement>,
-        VariantProps<typeof inputVariants> {
-    inputs: {};
+  extends InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {
+  inputs: {};
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-    const { inputs, className, variant } = props;
-    return (
-        <input
-            ref={ref}
-            {...inputs}
-            className={cn(inputVariants({ className, variant }))}
-        />
-    );
+  const { inputs, className, variant } = props;
+  return (
+    <input
+      ref={ref}
+      {...inputs}
+      className={cn(inputVariants({ className, variant }))}
+    />
+  );
 });
 
 export default Input;
